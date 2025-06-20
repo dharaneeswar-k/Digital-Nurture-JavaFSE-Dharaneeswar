da9e2f6705035b66d9a7126676a073bf8fd07ed7
DHARANEESWAR K <dharaneeswarkb@gmail.com>
2025-06-20 16:11:06 +0530

    Update Singleton.java

--- Week1_DesignPrinciplesAndPatterns/Exercise1_SingletonPattern/Code/Singleton.java
+++ Week1_DesignPrinciplesAndPatterns/Exercise1_SingletonPattern/Code/Singleton.java
@@ -1,14 +1,11 @@
 class Logger {
 
-    // Step 1: Create a private static instance of the Logger class
     private static Logger singleInstance;
 
-    // Step 2: Make the constructor private to prevent instantiation
     private Logger() {
         System.out.println("Logger instance created!");
     }
 
-    // Step 3: Provide a public static method to get the instance
     public static Logger getInstance() {
         if (singleInstance == null) {
             singleInstance = new Logger();
@@ -16,25 +13,20 @@ class Logger {
         return singleInstance;
     }
 
-    // Step 4: Sample method to demonstrate logging
     public void log(String message) {
         System.out.println("Log: " + message);
     }
 }
-// Main.java
 public class Singleton {
     public static void main(String[] args) {
 
-        // Get two instances of Logger
         Logger logger1 = Logger.getInstance();
         Logger logger2 = Logger.getInstance();
         
 
-        // Log messages using both instances
         logger1.log("This is the first log message.");
         logger2.log("This is the second log message.");
 
-        // Check if both instances are the same
         if (logger1 == logger2) {
             System.out.println("Both logger1 and logger2 are the same instance.");
         } else {
