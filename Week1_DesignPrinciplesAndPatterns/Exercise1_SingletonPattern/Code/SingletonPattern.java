2be3a337b8c4c1c25e9fc56e4244fadb03950e96
Dharaneeswar <dharaneeswar07@gmail.com>
2025-06-20 17:51:39 +0530

    new

---
+++ Week1_DesignPrinciplesAndPatterns/Exercise1_SingletonPattern/Code/SingletonPattern.java
@@ -0,0 +1,30 @@
+
+class logger{
+
+    private static logger singleInstance;
+    private logger(){
+        System.out.println("logger instance Created");
+    }
+
+    public static logger getInstance(){
+        if (singleInstance == null){
+            singleInstance = new logger();
+        }
+        return singleInstance;
+    }
+
+}
+public class SingletonPattern {
+
+    public static void main(String[] args) {
+        logger log1 = logger.getInstance();
+        logger log2 = logger.getInstance();
+
+        if (log1 == log2){
+            System.out.println("Both logger1 and logger2 are the same instance.");
+        }else{
+            System.out.println("Different instances were created. Singleton failed");
+        }
+    }
+
+}
