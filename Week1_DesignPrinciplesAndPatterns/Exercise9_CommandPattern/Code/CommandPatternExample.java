6a6e83b6eede48760467badbd0a44930c33ea810
Dharaneeswar <dharaneeswar07@gmail.com>
2025-06-21 21:23:05 +0530

    Updated repo with Exercise 9

---
+++ Week1_DesignPrinciplesAndPatterns/Exercise9_CommandPattern/Code/CommandPatternExample.java
@@ -0,0 +1,69 @@
+package Week1_DesignPrinciplesAndPatterns.Exercise9_CommandPattern.Code;
+
+interface Command {
+    void execute();
+}
+
+class Light {
+    public void turnOn() {
+        System.out.println("Light is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Light is OFF");
+    }
+}
+
+class LightOnCommand implements Command {
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOn();
+    }
+}
+
+class LightOffCommand implements Command {
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOff();
+    }
+}
+
+class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
+
+public class CommandPatternExample {
+    public static void main(String[] args) {
+        Light light1 = new Light();
+
+        Command lightOn = new LightOnCommand(light1);
+        Command lightOff = new LightOffCommand(light1);
+
+        RemoteControl remote = new RemoteControl();
+
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+        remote.setCommand(lightOff);
+        remote.pressButton();
+    }
+}
+
